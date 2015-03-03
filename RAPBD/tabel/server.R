c641ff2f9229b334b47264ceb6f65f279507a698
Ainun Najib <ainunnajib@gmail.com>
2015-03-03 09:35:52 +0800

    fix

--- RAPBD/tabel/server.R
+++ RAPBD/tabel/server.R
@@ -3,6 +3,16 @@ load("mata.anggaran.RData")
 
 shinyServer(function(input, output) {
   output$table <- renderDataTable({
-    mata.anggaran
+    data <- mata.anggaran
+    if (input$UrusanPemerintahan != "All"){
+      data <- data[data$UrusanPemerintahan == input$UrusanPemerintahan,]
+    }
+    if (input$Organisasi != "All"){
+      data <- data[data$Organisasi == input$Organisasi,]
+    }
+    if (input$KodeRekening != "All"){
+      data <- data[data$KodeRekening == input$KodeRekening,]
+    }
+    data
   })
 })
