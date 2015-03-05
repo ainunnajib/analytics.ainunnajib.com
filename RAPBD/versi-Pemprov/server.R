6732766b5360b3961682e87f243f9110c555e4d2
Ainun Najib <ainunnajib@gmail.com>
2015-03-05 12:05:15 +0800

    number sorting issue

--- RAPBD/versi-Pemprov/server.R
+++ RAPBD/versi-Pemprov/server.R
@@ -5,10 +5,10 @@ load("mata.anggaran.RData")
 shinyServer(function(input, output) {
   output$table <- renderDataTable({
     data <- mata.anggaran
-    data[ , BELANJABARANGDANJASA := format(BELANJABARANGDANJASA, scientific = FALSE, width = 15, big.mark = ".")]
-    data[ , BELANJAMODAL := format(BELANJAMODAL, scientific = FALSE, width = 15, big.mark = ".")]
-    data[ , BELANJAPEGAWAI := format(BELANJAPEGAWAI, scientific = FALSE, width = 15, big.mark = ".")]
-    data[ , TOTAL := format(TOTAL, scientific = FALSE, width = 15, big.mark = ".")]
+    data[ , BELANJABARANGDANJASA := paste("Rp.", format(BELANJABARANGDANJASA, scientific = FALSE, width = 15, big.mark = "."))]
+    data[ , BELANJAMODAL := paste("Rp.", format(BELANJAMODAL, scientific = FALSE, width = 15, big.mark = "."))]
+    data[ , BELANJAPEGAWAI := paste("Rp.", format(BELANJAPEGAWAI, scientific = FALSE, width = 15, big.mark = "."))]
+    data[ , TOTAL := paste("Rp.", format(TOTAL, scientific = FALSE, width = 15, big.mark = "."))]
     if (input$UrusanPemerintahan != "All"){
       data <- data[data$UrusanPemerintahan == input$UrusanPemerintahan,]
     }
